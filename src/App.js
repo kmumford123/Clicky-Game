30a5844038d95c4ca2af8af632ad2baa356f0981
Khamel <khamel.mumford@gmail.com>
2018-04-12 23:02:19 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends Component {
+
   render() {
+    // const indexes = math.random array.length
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const Footer = () => (
+
+<footer className="page-footer font-small blue pt-4 mt-4">
+
+    {/**Footer Links*/}
+    <div className="container-fluid text-center text-md-left">
+        <div className="row">
+
+            {/**First column*/}
+            <div className="col-md-6">
+                <h5 className="text-uppercase">Footer Content</h5>
+                <p>Here you can use rows and columns here to organize your footer content.</p>
+            </div>
+            {/**.First column */}
+
+            {/**Second column*/}
+            <div className="col-md-6">
+                <h5 className="text-uppercase">Links</h5>
+                <ul className="list-unstyled">
+                    <li>
+                        <a href="#!">Link 1</a>
+                    </li>
+                    <li>
+                        <a href="#!">Link 2</a>
+                    </li>
+                    <li>
+                        <a href="#!">Link 3</a>
+                    </li>
+                    <li>
+                        <a href="#!">Link 4</a>
+                    </li>
+                </ul>
+            </div>
+            {/**.Second column */}
+        </div>
+    </div>
+    {/**.Footer Links*/}
+
+    {/**Copyright*/}
+    <div className="footer-copyright py-3 text-center">
+        © 2018 Copyright:
+        <a href="https://mdbootstrap.com/material-design-for-bootstrap/"> MDBootstrap.com </a>
+    </div>
+    {/**.Copyright*/}
+
+</footer>
+)
+export default Footer;
